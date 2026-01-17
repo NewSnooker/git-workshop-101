88781a1783a4d2ae46838b010931228bd1a0b2ca
NewSnooker <146810213+NewSnooker@users.noreply.github.com>
2026-01-17 13:23:46 +0700

    Fix indentation in keep_vagetable.lua

--- keep_vagetable.lua
+++ keep_vagetable.lua
@@ -5,8 +5,8 @@ prompt.Triggered:Connect(function(player)
 	if player.Character and not player.Backpack:FindFirstChild(tool_vagetable.Name) 
 		and not player.Character:FindFirstChild(tool_vagetable.Name) then
 		
-		local tool = tool_vagetable:Clone()
-		player.Character.Humanoid:EquipTool(tool)
+			local tool = tool_vagetable:Clone()
+			player.Character.Humanoid:EquipTool(tool)
 	
 	end
 end)
